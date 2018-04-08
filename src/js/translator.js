a4c55e65e95fd0ec432193275496973669a08534
LFeh <hi@felipefialho.com>
2018-04-08 01:33:13 -0300

    Create a translator logical #2

--- src/js/translator.js
+++ src/js/translator.js
@@ -1,32 +1,24 @@
-const _ = require('lodash');
-const json = require('../library.json');
+import map from 'lodash/map';
+import forEach from 'lodash/forEach';
+import json from '../library.json';
 
-const delay = (function(){
-  let timer = 0;
-  return (callback, ms) => {
-    clearTimeout (timer);
-    timer = setTimeout(callback, ms);
-  };
-})();
-
-const Translator = () => {
-  const $translator = document.querySelectorAll('[data-translator="input"]')[0];
-  const $result = document.querySelectorAll('[data-translator="result"]')[0];
+const translator = () => {
+  const $translator = document.querySelector('[data-translator="input"]');
+  const $result = document.querySelector('[data-translator="result"]');
+  const $button = document.querySelector('[data-translator="button"]');
   const library = json[0];
-  let resultTranslator = [];
+  let resultTranslator, text; 
 
-  $translator.addEventListener('keydown', (e) => {
-    delay(() => { 
-      resultTranslator = [];
-      const words = $translator.value.toLowerCase().split(' ');
-      _.forEach(library, (value, key) => {
-        _.map(words, word => { 
-          word === key ? resultTranslator.push(value) : '';
-        });
-      });
-    }, 600);
-    $result.value = resultTranslator.join(' ');
+  $button.addEventListener('click', (e) => {
+    e.preventDefault();
+    resultTranslator = []; 
+    text = $translator.value.toLowerCase().split(' '); 
+    map(text, word => {
+      forEach(library, (value, key) => word === key ? word = value : word);
+      return resultTranslator.push(word);
+    });
+    $result.value = resultTranslator.join(' ') || 'Tradução';
   });
 };
 
-export { Translator };
+export { translator };
