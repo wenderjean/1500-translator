514e9c2f990479eb7862044cc876a1f172b84d95
LFeh <hi@felipefialho.com>
2018-04-10 19:01:25 -0300

    Code lint

--- src/js/convert.js
+++ src/js/convert.js
@@ -1,5 +1,4 @@
 export const removeAccent = text => {
-  if(!text.normalize) return text;
+  if (!text.normalize) return text;
   return text.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
-}
-
+};
