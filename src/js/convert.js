9f07e624999be92a50b5a75de4cc1127eadc36ac
luketevl <luketevl@gmail.com>
2018-04-10 18:08:26 -0300

    Adicionado função para não precisar informar acentuação

--- src/js/convert.js
+++ src/js/convert.js
@@ -1,5 +1,5 @@
 export const removeAccent = text => {
-  if(!str.normalize) return text
-  return text.normalize('NFD').replace(/[\u0300-\u036f]/g, "");
+  if(!text.normalize) return text;
+  return text.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
 }
 
